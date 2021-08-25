2e0abefa515965915ccf1b3109c7af2020e88040
Laura Alejandra Izquierdo Castro <alizeci17@gmail.com>
2021-08-24 21:42:46 -0500

    Ajuste endpoint

--- src/main/resources/public/js/apiclient.js
+++ src/main/resources/public/js/apiclient.js
@@ -3,14 +3,14 @@ var apiclient = (function () {
     var getInfoByStock = function (stock, timePeriod){
         
         /*Heroku Endpoint*/
-        /*axios.get('https://sparkwebapp-taller01.herokuapp.com/data?stock=' + stock+"&period="+timePeriod)
+        axios.get('https://sparkwebapp-taller01.herokuapp.com/data?stock=' + stock+"&period="+timePeriod)
         .then(response=> Module.showInfo(response.data))
-        .catch(error=> console.log(error));*/
+        .catch(error=> console.log(error));
         
         /*Localhost Endpoint*/
-        axios.get('http://localhost:4567/data?stock=' + stock+"&period="+timePeriod)
+        /*axios.get('http://localhost:4567/data?stock=' + stock+"&period="+timePeriod)
             .then(response=> Module.showInfo(response.data))
-            .catch(error=> console.log(error));
+            .catch(error=> console.log(error));*/
     };
 
 	return {
